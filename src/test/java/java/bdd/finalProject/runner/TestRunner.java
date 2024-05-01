3105228120d0a00fe01cc75c24d4bf70515ab314
MohammadArifzai <159109710+MohammadArifzai@users.noreply.github.com>
2024-05-01 15:56:08 -0600

    Final Project

---
+++ src/test/java/java/bdd/finalProject/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package java.bdd.finalProject.runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "java.bdd.finalProject.steps",
+        dryRun = true,
+        monochrome = false, //-> if true, this will avoid writing extra symbols and will show us a clear view of the output
+        plugin = {
+                "pretty",
+                "html:target/html_report/index.html",
+                "json:target/jsonReports/cucumber.json"
+        }
+
+)
+public class TestRunner extends AbstractTestNGCucumberTests  {
+}
+
+
---
+++ src/test/resources/config/application-config.feature
@@ -0,0 +1,5 @@
+
+
+retail.ui.url=https://dev.insurance.tekschool-students.com/
+retail.browser.type=chrome
+retail.browser.headless=true
