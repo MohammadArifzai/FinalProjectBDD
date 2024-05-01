1037daa5dab867fcfc4d7c7cb4bf741f83194f3d
MohammadArifzai <159109710+MohammadArifzai@users.noreply.github.com>
2024-05-01 15:56:08 -0600

    Final Project

---
+++ src/test/java/bdd/finalProject/pages/AccountPage.java
@@ -0,0 +1,14 @@
+package bdd.finalProject.pages;
+
+import org.openqa.selenium.By;
+
+public class AccountPage {
+    public static final By PRIMARY_ACCOUNTS_TITLE = By.xpath("//*[@id='root']/div/div[3]/div/div/div/h2");
+    public static final By PRIMARY_ACCOUNTS_ALL_ROWS = By.xpath("//*[@id='root']/div/div[3]/div/div/div/div[2]/table");
+
+    // option 2 for PRIMARY_ACCOUNTS_ALL_ROWS, if the current locator is not correct
+    // *[@id='root']/div/div[3]/div/div/div/div[2]/table/tbody
+
+    public static final By SHOW_PER_PAGE_OPTION = By.xpath("//*[@id='root']/div/div[3]/div/div/div/div[2]/div/div/div[3]/select");
+
+}
---
+++ src/test/java/bdd/finalProject/steps/AccountsSteps.java
@@ -0,0 +1,55 @@
+package bdd.finalProject.steps;
+
+
+import bdd.finalProject.pages.AccountPage;
+import bdd.finalProject.utility.SeleniumUtilities;
+
+import bdd.finalProject.base.BaseSetup;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+
+public class AccountsSteps extends SeleniumUtilities {
+
+
+
+    @Then("user should see 5 rows per page by default")
+    public void user_should_see_five_rows_per_page_by_default() {
+        Object AccountsPage;
+        List<WebElement> rows = BaseSetup.getDriver().findElements(AccountPage.PRIMARY_ACCOUNTS_ALL_ROWS);
+        Assert.assertEquals(5, rows.size());
+
+    }
+
+    @When("user selects the show per page option as (.*)$")
+    public void user_selects_the_show_per_page_option_as(String selectedNumber) {
+        selectFromDropDownByValue(AccountPage.SHOW_PER_PAGE_OPTION, selectedNumber);
+    }
+
+    @Then("user should see (.*) rows per page$")
+    public void user_should_see_rows_per_page(String expectedRows) {
+        int actualRows = BaseSetup.getDriver().findElements(AccountPage.PRIMARY_ACCOUNTS_ALL_ROWS).size();
+        // Integer.parseInt() is a method that converts a String to an integer. It takes a String argument and returns an int value.
+        Assert.assertEquals(Integer.parseInt(expectedRows), actualRows);
+
+    }
+
+
+    @Then("user clicks on the Accounts button")
+    public void userClicksOnTheAccountsButton() {
+    }
+
+    @And("user enters valid username {string} and valid password {string}")
+    public void userEntersValidUsernameAndValidPassword(String arg0, String arg1) {
+        
+    }
+
+    @Then("user should be navigated to Customer Service Portal")
+    public void userShouldBeNavigatedToCustomerServicePortal() {
+    }
+}
